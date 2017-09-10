30fe8333b4399beb2ab1d0a0500836ec799305dc
Pavel Pershko <thrashmetalfan98@gmail.com>
2017-09-10 21:38:03 +0300

    Design structure was changed, design was adopted for mobiles; created pattern definition; game complexity was reduced.

--- static/drawing.js
+++ static/drawing.js
@@ -1,7 +1,37 @@
-function drawLine(x1, y1, x2, y2, context) {
+function hide(jqelem) {
+    jqelem
+        .css('transform', 'scaleY(0)')
+        .css('height', '0');
+}
+
+function show(jqelem, height = 'auto') {
+    jqelem
+        .css('height', height)
+        .css('transform', 'scaleY(1)');
+}
+
+function openSettings(event) {
+    show($("#settings"));
+    $('#setBtn')
+        .unbind('click', openSettings)
+        .bind('click', closeSettings);
+    event.stopPropagation()
+}
+
+function closeSettings(event) {
+    hide($("#settings"));
+    $('#setBtn')
+        .unbind('click', closeSettings)
+        .bind('click', openSettings);
+    event.stopPropagation()
+}
+
+function drawLine(x1, y1, x2, y2, colour, context) {
     context.beginPath();
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
+    context.lineWidth = 2;
+    context.strokeStyle = colour;
     context.stroke();
 }
 
@@ -15,22 +45,32 @@ function drawCicle(x, y, r, colour, context) {
 
 var usertaps = $("#usertaps")[0];
 var dx = usertaps.width / 10;
+var tap_line_length = dx * 8;
 var y = usertaps.height / 2;
 var userCanvContext = usertaps.getContext('2d');
 
 function drawTapCycle(part, valid) {
-    drawCicle(dx + dx * 8 * part, y, 6, valid ? 'lime' : 'red', userCanvContext);
+    drawCicle(dx + tap_line_length * part, y, 6, valid ? 'lime' : 'red', userCanvContext);
 }
 
-function clearTapLine(signature = {note_value: 4, beats_num: 4}) {
+function clearTapLine(signature, pattern) {
     userCanvContext.clearRect(0, 0, usertaps.width, usertaps.height);
     var lip = 30; //px
-    drawLine(dx - lip, y, dx * 9 + lip, y, userCanvContext);
-    var d = dx * 8 / signature.beats_num;
-    for (var i = 0, width = 8; i < signature.beats_num + 1; ++i) {
-        var x = dx + d * i;
-        drawLine(x, y - width, x, y + width, userCanvContext);
-    }
-}
+    drawLine(dx - lip, y, dx * 9 + lip, y, 'black', userCanvContext);
+    var d = tap_line_length / signature.beats_num;
 
-clearTapLine();
+    function drawMetrBeats(width = 3) {
+        for (var i = 0; i < signature.beats_num + 1; ++i) {
+            var x = dx + d * i;
+            drawLine(x, y - width, x, y + width, 'black', userCanvContext);
+        }
+    }
+    function drawPattern(pattern, width = 8) {
+        for (var i in pattern) {
+            var x = pattern[i] * tap_line_length + dx;
+            drawLine(x, y - width, x, y + width, 'LightSeaGreen', userCanvContext);
+        }
+    }
+    drawPattern(pattern);
+    drawMetrBeats();
+}
--- static/rhythm_trainer.js
+++ static/rhythm_trainer.js
@@ -1,4 +1,14 @@
-function TapInspector(interval, signature) {
+function TapInspector(interval, signature, pattern) {
+    function search(val, left = 0, arr = pattern, right = arr.length - 1) { //it must be replaced on O(1) search !!!!!!
+        while (left < right) {
+            var mid = (left + right) / 2;
+            mid = Math.floor(mid);
+            if (val > arr[mid]) left = mid + 1;
+            else right = mid;
+        }
+        return val - arr[left] < arr[right] - val ? left : right;
+    }
+
     var startTime = Date.now();
     interval = Math.round(interval);
     const duplicate_part = 0.04; //experimentally; it must fill only line
@@ -9,10 +19,11 @@ function TapInspector(interval, signature) {
         'super': 2
     }[$('input:radio[name="complexity"]:checked')[0].value]; //ms
     var lastTaps = [];
+    var prevBeatNum = 0;
     this.tap = function () {
         var spentTime = Date.now() - startTime;
-        var mod = spentTime % interval;
-        var delta = Math.min(interval - mod, mod);
+        prevBeatNum = search(spentTime, prevBeatNum);
+        var delta = Math.abs(pattern[prevBeatNum] - spentTime);
         var part = spentTime / interval;
         part /= signature.beats_num;
         if (part > 1 - duplicate_part) {
@@ -23,6 +34,7 @@ function TapInspector(interval, signature) {
     };
     this.reset = function () {
         lastTaps = [];
+        prevBeatNum = 0;
         startTime = Date.now();
     };
     this.drawLastTaps = function () {
@@ -34,33 +46,59 @@ function TapInspector(interval, signature) {
 
 const CLICK = 'ontouchstart' in window ? 'touchstart' : 'click';
 
-function Cycle(interval, signature = {note_value: 4, beats_num: 4}) {
+function Cycle(interval, signature = {note_value: 4, beats_num: 4}, pattern = null) {
+    if (pattern === null) {
+        pattern = {};
+        for (var i = 0; i < signature.beats_num; ++i) {
+            pattern[i] = [1];
+        }
+    }
     interval /= signature.note_value / 4;
+    const duration = interval * signature.beats_num;
+
+    function getLinearPattern(pattern, interval, time = 0) {
+        var lin_pat = {'time': [], 'part': []};
+        for (var part_num in pattern) {
+            var subpattern = pattern[part_num];
+            if (subpattern[0] === 1) {
+                var beat_time = time + part_num * interval;
+                lin_pat['time'].push(beat_time);
+                lin_pat['part'].push(beat_time / duration);
+            } else {
+                var new_lin_pat = getLinearPattern(subpattern[1], interval / subpattern[0], time + part_num * interval);
+                for (var key in lin_pat) lin_pat[key] = lin_pat[key].concat(new_lin_pat[key]);
+            }
+        }
+        return lin_pat;
+    }
+
+    pattern = getLinearPattern(pattern, interval);
     var metr = new Metronome(interval, signature);
-    var insp = new TapInspector(interval, signature);
+    var insp = new TapInspector(interval, signature, pattern['time']);
     var intervalId = null;
     this.play = function () {
-        clearTapLine(signature);
+        clearTapLine(signature, pattern['part']);
         insp.drawLastTaps();
         insp.reset();
         metr.play();
     };
+
     function tap(event) {
         var ignoredTags = new Set(['INPUT', 'BUTTON']);
         if (ignoredTags.has(event.target.tagName)) return;
         insp.tap();
     }
-
+    var tapzone = $(document);
     this.start = function () {
-        $("#tapzone").bind(CLICK, tap);
-        $('body').bind('keydown', tap);
+        tapzone.bind(CLICK, tap);
+        $(document).bind('keydown', tap);
         this.play();
         intervalId = setInterval(this.play, interval * signature.beats_num);
     };
     this.stop = function () {
         metr.stop();
-        $("#tapzone").unbind(CLICK, tap);
-        $("body").unbind('keydown', tap);
+        tapzone.unbind(CLICK, tap);
+        $(document).unbind('keydown', tap);
         clearInterval(intervalId);
     };
 }
@@ -75,14 +113,14 @@ var game = {
                 .unbind('click', stopGame)
                 .html('<b>Play</b>')
                 [0].onclick = startGame; // .bind doesn't work
-            this.cell.css('height', '0px');
+            hide(this.cell);
         },
         stop: function () {
             this.elem
                 .unbind('click', startGame)
                 .html('<b>Stop</b>')
                 [0].onclick = stopGame; // .bind doesn't work
-            this.cell.css('height', '20px');
+            show(this.cell, '20px');
         }
     },
     start: function () {
@@ -96,9 +134,10 @@ var game = {
 
         this.btn.stop();
         var tempo = parseAndLimit($('form[name="tempo"] > input')[0], 10, 60, 280);
+        var form = $('form[name="signature"]');
         var signature = {
-            beats_num: parseAndLimit($('form[name="signature"] > input[name="beats_num"]')[0], 1, 4, 64),
-            note_value: parseAndLimit($('form[name="signature"] > input[name="note_value"]')[0], 1, 4, 64)
+            beats_num: parseAndLimit(form.find('input[name="beats_num"]')[0], 1, 4, 64),
+            note_value: parseAndLimit(form.find('input[name="note_value"]')[0], 1, 4, 64)
         };
         this.cycle = new Cycle(1000 * 60 / tempo, signature);
         this.cycle.start();
